3d99924501a0029362d68f33f2b0f4889d9a7b6c
Chris <34993144+Mesiow@users.noreply.github.com>
2022-08-27 18:08:27 -0400

    map first 2 rows of primary opcodes

--- PSEmu/Core/Opcodes.cpp
+++ PSEmu/Core/Opcodes.cpp
@@ -10,4 +10,13 @@ void Cpu::map_opcodes()
 	primary_lut[0x05] = Instruction{ b(&Cpu::bne), 1 };
 	primary_lut[0x06] = Instruction{ b(&Cpu::blez), 1 };
 	primary_lut[0x07] = Instruction{ b(&Cpu::bgtz), 1 };
+
+	primary_lut[0x08] = Instruction{ b(&Cpu::addi), 1 };
+	primary_lut[0x09] = Instruction{ b(&Cpu::addiu), 1 };
+	primary_lut[0x0A] = Instruction{ b(&Cpu::slti), 1 };
+	primary_lut[0x0B] = Instruction{ b(&Cpu::sltiu), 1 };
+	primary_lut[0x0C] = Instruction{ b(&Cpu::andi), 1 };
+	primary_lut[0x0D] = Instruction{ b(&Cpu::ori), 1 };
+	primary_lut[0x0E] = Instruction{ b(&Cpu::xori), 1 };
+	primary_lut[0x0F] = Instruction{ b(&Cpu::lui), 1 };
 }
